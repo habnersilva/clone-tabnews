685ac4a01206c3d63572f4ff8d47a04aaa2e316c
Habner Silva <contato@habnersilva.com.br>
2024-07-04 00:59:21 +0000

    adiciona scritps `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Bom dia! Te amo amor. Você me ama dá uma risadinha! <small>Habner</small></h1>
+function Home() {
+  return (
+    <h1>
+      Bom dia! Te amo amor. Você me ama dá uma risadinha! <small>Habner</small>
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
