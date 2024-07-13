d31037fa5cc3896d1ce9e7f7c18f14d6b1365751
Habner Silva <contato@habnersilva.com.br>
2024-07-13 13:22:01 +0000

    finish `/api/v1/status`endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionResultValue =
+    databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsResultValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsResultValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    chave: "Olá",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionResultValue,
+        max_connections: parseInt(databaseMaxConnectionsResultValue),
+        opened_connections: databaseOpenedConnectionsResultValue,
+      },
+    },
   });
 }
 
